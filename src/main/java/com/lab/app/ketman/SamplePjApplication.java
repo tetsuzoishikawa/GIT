4345438b089052f401cfa5341f69bb64be25b4f4
ishikawa <ishikawa@engineers.co.jp>
2021-01-20 13:15:29 +0900

    SamplePj push

---
+++ src/main/java/com/lab/app/ketman/SamplePjApplication.java
@@ -0,0 +1,14 @@
+package com.lab.app.ketman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePjApplication.class, args);
+//		SpringApplication.run(SampleController.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/app/ketman/SamplePjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.app.ketman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplePjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
